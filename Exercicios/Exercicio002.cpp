558759ffab31edfffb3877eec5fd4ade42b7cbf9
Barbara Gomes <barbara_gomes97@outlook.com>
2020-06-04 13:35:22 -0300

    Exercicio002
    Boas vindas, usuário

---
+++ Exercicios/Exercicio002.cpp
@@ -0,0 +1,10 @@
+//Ler um nome e das as boas vindas
+#include <iostream>
+using namespace std;
+int main(){
+    string nome;
+    cout << "Ola! Digite o seu nome: ";
+    cin >> nome;
+    cout <<"Seja bem vindo(a), "<< nome << "!";
+    return 0;
+}
