ccb9332b34e38171c1942f27372649cfbb4bc9b6
Barbara Gomes <barbara_gomes97@outlook.com>
2020-06-04 13:49:15 -0300

    Exercicio003
    Input de 2 números e sua soma

---
+++ Exercicios/Exercicio003.cpp
@@ -0,0 +1,12 @@
+// Ler dois números e mostrar sua soma
+#include <iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout << "Ola! Digite um numero: ";
+    cin >> n1;
+    cout << "Digite mais um numero: ";
+    cin >> n2;
+    cout << "A soma entre " << n1 << " e " << n2 << " equivale a " << n1+n2 << "!";
+    return 0;
+}
