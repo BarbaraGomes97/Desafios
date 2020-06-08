ae8e911df0f9322c6374f36dfc957673a81e1706
Barbara Gomes <barbara_gomes97@outlook.com>
2020-06-08 15:28:33 -0300

    Update Exercicio011.py

--- Python/Exercicio011.py
+++ Python/Exercicio011.py
@@ -2,6 +2,6 @@
 import colorama
 colorama.init()
 print("\033[36;1m- Calculadora comercial -\033[m")
-valor = float(input("Digite o valor do produto: R$"))
+valor = float(input('Digite o valor do produto: R$'))
 desc = valor*5/100
-print("Preço final do produto: R${:.2f}".format(valor-desc))
+print('Preço final do produto: R${:.2f}'.format(valor-desc))
